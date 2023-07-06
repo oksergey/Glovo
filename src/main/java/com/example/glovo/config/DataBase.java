1e9bb6968f3f448bbbcfe0c14056d59596fa637e
Zakushniak Sergii <ok.sergey@gmail.com>
2023-07-06 13:17:51 +0300

    hw21 db (all commit in master)

---
+++ src/main/java/com/example/glovo/config/DataBase.java
@@ -0,0 +1,23 @@
+package com.example.glovo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataBase {
+
+//    @Bean
+//    public DataSource myPostgres() {
+//        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//        dataSource.setDriverClassName();
+//        dataSource.setUrl(url);
+//        dataSource.setUsername("username");
+//        dataSource.setPassword("password");
+//
+//
+//        return dataSource;
+//    }
+}
